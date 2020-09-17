5117e20efd9bfe7f0d00429471264ca8e6a473b2
popote1 <yvan.bladet@gmail.com>
2020-09-17 18:03:37 +0200

    Added Work Ecole17_09_2020

---
+++ Assets/scripte/ZerglingHolder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZerglingHolder : MonoBehaviour
+{
+    [Header("StartStats")]
+    [Range(1, 100)] public int PV=100;
+    [Range(1, 100)] public int Food=50;
+    [Range(1, 100)] public int Power=75;
+    private Zergling _zergling;
+    void Start()
+    {
+        _zergling = new Zergling(PV, Food, Power);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripte/ZerglingStat.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZerglingStat : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
+
+public class Zergling
+{
+    
+    private int _health;
+    public int Health
+    {
+        get => _health ;
+        set
+        {
+            _health = Mathf.Clamp(value,0,100);
+        }
+    }
+
+    private int _food;
+    public int Food
+    {
+        get => _food;
+        set => _food = Mathf.Clamp(value,0,100);
+    }
+
+    private int _power;
+
+    public int Power
+    {
+        get => _power;
+        set => _power= Mathf.Clamp(value,0,100);
+    }
+
+    private int _exostStat;
+
+    public int ExostStat
+    {
+        get => _exostStat;
+        set => _exostStat = value;
+    }
+
+    public Zergling(int health, int Food, int Power)
+    {
+        _health = health;
+        _food = Food;
+        _power = Power;
+    }
+}
